d028d2814527ff453378f2e7c55b54ee7c292bb6
Steven Lara <steven.carcamo2019adoc@gmail.com>
2020-07-10 16:55:26 -0300

    I created NavbarController and modified routes/web.php changing its respective controller@method as second parameter

---
+++ app/Http/Controllers/NavbarController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NavbarController extends Controller
+{
+    function homeView(){
+        return view('home');
+    }
+    function ourAlliesView(){
+        return view('nuestros_aliados');
+    }
+    function contactUsView(){
+        return view('contactanos');
+    }
+    function ourHistoryView(){
+        return view('nuestra_historia');
+    }
+    function ourPhilosophyView(){
+        return view('nuestra_filosofia');
+    }
+    function multiplyEffectView(){
+        return view('efecto_multiplicador');
+    }
+    function ourTeamView(){
+        return view('nuestro_equipo');
+    }
+    function ourProgramView(){
+        return view('nuestro_programa');
+    }
+    function qualityFocusView(){
+        return view('enfoque_en_la_calidad');
+    }
+    function impactView(){
+        return view('impacto');
+    }
+    function hilasalView(){
+        return view('cs_hilasal');
+    }
+    function adocView(){
+        return view('cs_adoc');
+    }
+    function sanMiguelitoView(){
+        return view('cs_fam_sanmiguelito');
+    }
+    function pomaView(){
+        return view('cs_fpoma');
+    }
+    function merletView(){
+        return view('cs_merlet');
+    }
+    function jupaView(){
+        return view('cs_jupa');
+    }
+    function raicesView(){
+        return view('cs_raices');
+    }
+    function grupoQView(){
+        return view('cs_grupoq');
+    }
+    function ciudadDelNinoView(){
+        return view('cs_fam_cdn');
+    }
+    function proviviendaView(){
+        return view('cs_fpro');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,33 +3,33 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'superate'], function () {
-    Route::view('/', 'home')->name('home');
-    Route::view('nuestros-aliados', 'nuestros_aliados')->name('nuestros-aliados');
-    Route::view('contactanos', 'contactanos')->name('contactanos');
+    Route::get('/', 'NavbarController@homeView')->name('home');
+    Route::get('nuestros-aliados', 'NavbarController@ourAlliesView')->name('nuestros-aliados');
+    Route::get('contactanos', 'NavbarController@contactUsView')->name('contactanos');
 
     Route::group(['prefix' => 'quienes-somos'], function () {
-        Route::view('nuestra-historia', 'nuestra_historia')->name('nuestra-historia');
-        Route::view('nuestra-filosofia', 'nuestra_historia')->name('nuestra-filosofia');
-        Route::view('efecto-multiplicador', 'efecto_multiplicador')->name('efecto-multiplicador');
-        Route::view('nuestro-equipo', 'nuestro_equipo')->name('nuestro-equipo');
+        Route::get('nuestra-historia', 'NavbarController@ourHistoryView')->name('nuestra-historia');
+        Route::get('nuestra-filosofia', 'NavbarController@ourPhilosophyView')->name('nuestra-filosofia');
+        Route::get('efecto-multiplicador', 'NavbarController@multiplyEffectView')->name('efecto-multiplicador');
+        Route::get('nuestro-equipo', 'NavbarController@ourTeamView')->name('nuestro-equipo');
     });
 
     Route::group(['prefix' => 'que-hacemos'], function () {
-        Route::view('nuestro-programa', 'nuestro_programa')->name('nuestro-programa');
-        Route::view('enfoque-en-la-calidad', 'enfoque_en_la_calidad')->name('enfoque-en-la-calidad');
-        Route::view('impacto', 'impacto')->name('impacto');
+        Route::get('nuestro-programa', 'NavbarController@ourProgramView')->name('nuestro-programa');
+        Route::get('enfoque-en-la-calidad', 'NavbarController@qualityFocusView')->name('enfoque-en-la-calidad');
+        Route::get('impacto', 'NavbarController@impactView')->name('impacto');
     });
 
     Route::group(['prefix' => 'nuestros-centros'], function () {
-        Route::view('cs-hilasal', 'cs_hilasal')->name('cs-hilasal');
-        Route::view('cs-adoc', 'cs_adoc')->name('cs-adoc');
-        Route::view('cs-fam-san-miguelito', 'cs_fam_sanmiguelito')->name('cs-fam-san-miguelito');
-        Route::view('cs-fpoma', 'cs_fpoma')->name('cs-fpoma');
-        Route::view('cs-merlet', 'cs_merlet')->name('cs-merlet');
-        Route::view('cs-jupa', 'cs_jupa')->name('cs-jupa');
-        Route::view('cs-raices', 'cs_raices')->name('cs-raices');
-        Route::view('cs-grupo-Q', 'cs_grupoq')->name('cs-grupoQ');
-        Route::view('cs-fam-cdn', 'cs_fam_cdn')->name('cs-fam-cdn');
-        Route::view('cs-provivienda', 'cs_fpro')->name('cs-provivienda');
+        Route::get('cs-hilasal', 'NavbarController@hilasalView')->name('cs-hilasal');
+        Route::get('cs-adoc', 'NavbarController@adocView')->name('cs-adoc');
+        Route::get('cs-fam-san-miguelito', 'NavbarController@sanMiguelitoView')->name('cs-fam-san-miguelito');
+        Route::get('cs-fpoma', 'NavbarController@pomaView')->name('cs-fpoma');
+        Route::get('cs-merlet', 'NavbarController@merletView')->name('cs-merlet');
+        Route::get('cs-jupa', 'NavbarController@jupaView')->name('cs-jupa');
+        Route::get('cs-raices', 'NavbarController@raicesView')->name('cs-raices');
+        Route::get('cs-grupo-Q', 'NavbarController@grupoQView')->name('cs-grupoQ');
+        Route::get('cs-fam-cdn', 'NavbarController@ciudadDelNinoView')->name('cs-fam-cdn');
+        Route::get('cs-provivienda', 'NavbarController@proviviendaView')->name('cs-provivienda');
     });
 });
