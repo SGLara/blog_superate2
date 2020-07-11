74c78fdf763a36c8e02bd61e6e5ef30a266ce1e8
Steven Lara <steven.carcamo2019adoc@gmail.com>
2020-07-11 02:49:33 -0300

    I finished creating my navbar with pure CSS code and add superate image to every single view

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,23 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{-- <link rel="stylesheet" href="{{asset('css/navbarStyle.css')}}"> --}}
+    <link rel="stylesheet" href="{{asset('css/navbarStyle2.css')}}">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <title>@yield('title', 'withoutTitle')</title>
 </head>
 
 <body>
-    <div class="container">
-        <header class="header">
-            {{-- <img class="superate_logo" src="img/superate_logo.png" alt="Supérate_Logo"> --}}
-            @include('components.navbar')
-        </header>
-        <main class="content">
-            @yield('content')
-        </main>
-    </div>
+    <header class="header">
+        <div>
+            <img class="superate_logo" src="{{asset('img/superate_logo.png')}}" alt="Supérate_Logo">
+        </div>
+        @include('components.navbar')
+    </header>
+    <main class="content">
+        @yield('content')
+    </main>
 
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
