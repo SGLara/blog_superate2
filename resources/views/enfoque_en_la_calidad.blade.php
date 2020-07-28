36f7d6fad3ea8497a390de2d35cb922721d42dc8
Steven Lara <steven.carcamo2019adoc@gmail.com>
2020-07-27 18:09:01 -0600

    I finished focus-on-quality view

--- resources/views/enfoque_en_la_calidad.blade.php
+++ resources/views/enfoque_en_la_calidad.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.layout')
 
+@section('style_section')
+<link rel="stylesheet" href="{{asset('css/enfoque_en_la_calidad.css')}}">
+@endsection
 @section('title','Enfoque en la Calidad')
-
 @section('content')
-    <h1>Enfoque en la Calidad</h1>
+<div class="focus-on-quality">
+    <div class="title">
+        <h1>Enfoque en la Calidad</h1>
+    </div>
+    <div class="img"><img src="{{ asset('img/what-we-do/focus-on-quality/focus.jpg') }}" alt="photo"></div>
+    <div class="content">
+        <p>El Programa ¡Supérate! posee un enfoque en la calidad educativa, entendida como el resultado de la
+            articulación y mejora constante de sus elementos principales como son el compromiso empresarial, docentes y
+            directores competentes, manuales y programas curriculares adecuados, jóvenes con deseos de superación y la
+            evidencia externa del aprendizaje (certificaciones y evaluaciones).
+        </p>
+    </div>
+</div>
 @endsection
